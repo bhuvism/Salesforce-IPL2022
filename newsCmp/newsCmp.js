9de82511bea4979ac7e293364505d2908cfdfd05
Bhuvan Jain SM <bhuvan.jain@tavant.com>
2023-01-14 10:44:21 +0530

    done

---
+++ newsCmp/newsCmp.js
@@ -0,0 +1,13 @@
+import { LightningElement, wire } from 'lwc';
+import getNews from '@salesforce/apex/IplServices.getNews';
+
+export default class NewsCmp extends LightningElement {
+    news;
+    @wire(getNews)
+    wiredNews({error,data}){
+        if(data){
+            this.news = data;
+            console.log(this.news);
+        }
+    }
+}
